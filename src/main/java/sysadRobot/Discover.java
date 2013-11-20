27c3bc4816600c4e25d67102f9cf54590d6eeace
genmymodel <help@genmymodel.com>
2013-11-20 14:43:45 +0100

    commit by genmymodel from www.genmymodel.com: v20131120_144344

---
+++ src/main/java/sysadRobot/Discover.java
@@ -0,0 +1,44 @@
+package sysadRobot;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Discover
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Discover(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void grap() {
+		// TODO : to implement	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void calculateRoute() {
+		// TODO : to implement	
+	}
+	
+}
+
---
+++ src/main/java/sysadRobot/Household.java
@@ -0,0 +1,42 @@
+package sysadRobot;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Household
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public boolean inFrontOfWall;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Household(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void vacuum() {
+		// TODO : to implement	
+	}
+	
+}
+
